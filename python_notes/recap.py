71db8170c67e4b7499f43e8d63d980dcb5b6a996
Katy Long <katy.long@wearecodenation.com>
2022-06-27 17:05:48 +0100

    added python notes folder

---
+++ python_notes/recap.py
@@ -0,0 +1,15 @@
+# this is how to show information
+
+print("this is my file")
+
+greeting = "Hello everyone"
+
+print(greeting)
+
+print("This is a string for displaying characters")
+print("1234") #this is a string
+print(1234+1) #this is an integer/whole number
+print(12.34) #this is a float
+print(True) #one of the booolean data values
+print(False) #one of the booleans
+print(None) #none - blank/null data
